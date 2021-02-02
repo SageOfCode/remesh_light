97bd1c5c0ae29c10f25b617a5a02d41f6ff5ba67
Sage Gonzales <sagegonzales15@gmail.com>
2021-02-02 15:02:08 -0700

    Migrate thought and message to schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_02_214828) do
+ActiveRecord::Schema.define(version: 2021_02_02_220108) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,4 +21,22 @@ ActiveRecord::Schema.define(version: 2021_02_02_214828) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "messages", force: :cascade do |t|
+    t.string "text"
+    t.bigint "conversation_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["conversation_id"], name: "index_messages_on_conversation_id"
+  end
+
+  create_table "thoughts", force: :cascade do |t|
+    t.string "text"
+    t.bigint "message_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["message_id"], name: "index_thoughts_on_message_id"
+  end
+
+  add_foreign_key "messages", "conversations"
+  add_foreign_key "thoughts", "messages"
 end
