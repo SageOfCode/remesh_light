b06c6d83ce76c4daedee10c75e3148d813bd10cd
Sage Gonzales <sagegonzales15@gmail.com>
2021-02-02 16:44:26 -0700

    Add controller actions for new and create convo

--- app/controllers/conversations_controller.rb
+++ app/controllers/conversations_controller.rb
@@ -2,4 +2,18 @@ class ConversationsController < ApplicationController
   def index 
     @conversations = Conversation.all
   end 
+
+  def new 
+    @conversation = Conversation.new
+  end
+  
+  def create
+    @conversation = Conversation.create(convo_params)
+    redirect_to conversations_path
+  end
+
+  private
+  def convo_params
+    params.require(:conversation).permit(:title)
+  end
 end
