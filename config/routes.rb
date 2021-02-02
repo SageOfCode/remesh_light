31919194bcf765fe9678ef511dfd2f222b56803f
Sage Gonzales <sagegonzales15@gmail.com>
2021-02-02 16:45:03 -0700

    Add new create and show routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :conversations, only: [:index]
+  resources :conversations, only: [:new, :index, :show, :create]
 end
