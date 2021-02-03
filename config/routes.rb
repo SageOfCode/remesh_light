2ff38ce106dc78f7d4651e70713212353557590e
Sage Gonzales <sagegonzales15@gmail.com>
2021-02-02 21:43:27 -0700

    Add route for welcome page

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root "welcome#index"
   resources :conversations, only: [:new, :index, :show, :create] do
     resources :messages, only: [:new, :create] do 
       resources :thoughts, only: [:new, :create]
