45d62b131ea3548987ed8cf5397e4524748b1be4
Sage Gonzales <sagegonzales15@gmail.com>
2021-02-02 21:35:44 -0700

    Add nested resources for messages and thoughts

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :conversations, only: [:new, :index, :show, :create]
+  resources :conversations, only: [:new, :index, :show, :create] do
+    resources :messages, only: [:new, :create] do 
+      resources :thoughts, only: [:new, :create]
+    end
+  end
 end
